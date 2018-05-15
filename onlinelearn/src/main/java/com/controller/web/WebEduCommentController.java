cae94f361c89ee14855a33e8acdb27adad7685b7
zhaoshishuang <zhaoshishuang@LAPTOP-7HJ83PSF>
2018-05-15 08:39:20 +0800

    ssss

--- onlinelearn/src/main/java/com/controller/web/WebEduCommentController.java
+++ onlinelearn/src/main/java/com/controller/web/WebEduCommentController.java
@@ -1,10 +1,23 @@
 package com.controller.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bean.EduComment;
+import com.service.EduCommentService;
 
 @Controller
 public class WebEduCommentController {
 
-	
-	
+	@Autowired
+	private EduCommentService eduCommentService;
+	@RequestMapping("/addArtucleqt/{eduComment}")
+	public ModelAndView addArtucleqt(@PathVariable("eduComment")EduComment eduComment){
+		ModelAndView mv = new ModelAndView();
+		
+		return mv;
+	}
 }
